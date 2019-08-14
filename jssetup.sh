d6fd0c7c54ad1860703e52de028f6878e1457083
Quoc Vu <darthvu@gmail.com>
2019-08-13 19:32:02 -0700

    adding prettier config

--- jssetup.sh
+++ jssetup.sh
@@ -12,7 +12,7 @@ do
     curl -s https://raw.githubusercontent.com/quocvu/dotfiles/master/$f -o $f
 done
 
-npm install -s -d babel-eslint eslint eslint-config-airbnb eslint-plugin-import prettier eslint-config-prettier eslint-plugin-prettier mocha rewire should sinon
+npm install -s -D babel-eslint eslint eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react prettier eslint-config-prettier eslint-plugin-prettier mocha rewire should sinon
 
 echo "All done!"
 exit 0
