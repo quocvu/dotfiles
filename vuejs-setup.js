d28508b3ef2d51251b4a0a981c26f096c1fd0df7
Quoc Vu <darthvu@gmail.com>
2019-09-24 12:24:57 -0700

    tweak setup scripts

--- vuejs-setup.js
+++ vuejs-setup.js
@@ -4,7 +4,7 @@ echo "==========================================="
 echo "  Setting up your Node project"
 echo "==========================================="
 
-FILES=( ".editorconfig" ".eslintrc" "jest.config.js" ".npmrc" ".prettierrc" )
+FILES=( ".editorconfig" ".eslintrc" ".npmrc" ".prettierrc" )
 
 for f in "${FILES[@]}"
 do
@@ -14,7 +14,7 @@ done
 
 curl -s https://raw.githubusercontent.com/github/gitignore/master/Node.gitignore -o .gitignore
 
-npm install -s -D babel-eslint babel-jest babel-plugin-rewire eslint eslint-config-prettier eslint-plugin-prettier jest jest-extended prettier
+npm install -s -D babel-eslint babel-jest eslint eslint-config-prettier eslint-plugin-prettier prettier
 npx install-peerdeps --dev eslint-config-airbnb
 
 echo "All done!"
